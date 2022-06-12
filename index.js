9d231d2a84998aff7b057cba12845beaa7c501bc
Bonface Masira <bonfacemasira@gmail.com>
2022-06-12 22:54:43 +0300

    commit tree 398ecc05e476d7030df00e9cebd1c27901e49d08

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const cardMessage =[];
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++) {
+        cardMessage.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);        
+    };
+    return cardMessage;
+}
+
+function countDown() {
+    let count = 10;
+    while (count >= 0) {
+      console.log(count--);
+    }
+  }
